0994cc4a6663e08037dc4a731fcf52ae38b574f9
Jeff <jeffreysaa@outlook.com>
2017-06-14 12:55:42 -0500

    Search categorie and subcategorie fixed

--- app/controllers/api/v1/categories/categories_controller.rb
+++ app/controllers/api/v1/categories/categories_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Categories::CategoriesController < ApplicationController
 		if charge_exist(@current_user, Constants::FREPI_ADMIN)
 			categorie = Categorie.new(categorie_params)
 			categorie.downcase_fields
-			if categorie.save
+			if categorie
 				render :json => categorie, status: :ok
 			else
 				render :json => categorie, status: :bad_request
@@ -14,12 +14,12 @@ class Api::V1::Categories::CategoriesController < ApplicationController
 	end
 
 	def index
-		render :json => {model: Categorie.all.where(categorie_type: 1)}, status: :ok
+		render :json => {model: Categorie.all.where(categorie_type: params[:type])}, status: :ok
 	end
 
 	def search_subcategorie
 		q = params[:search].downcase
-		response = Categorie.where('name LIKE ?', "%#{q}%")
+		response = Categorie.where('name LIKE ?', "%#{q}%").where(categorie_type: params[:type])
 		render :json => response, status: :ok
 	end
 
