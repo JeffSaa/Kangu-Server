95d7396df210aad59f24fc0844f88630eb7cec87
Jeff <jeffreysaa@outlook.com>
2017-06-14 13:22:30 -0500

    Create, list categories ready

--- app/controllers/api/v1/categories/categories_controller.rb
+++ app/controllers/api/v1/categories/categories_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::Categories::CategoriesController < ApplicationController
-	before_action :validate_authentification_token, :except => [:index]
+	before_action :validate_authentification_token
 
 	def create
 		if charge_exist(@current_user, Constants::FREPI_ADMIN)
 			categorie = Categorie.new(categorie_params)
 			categorie.downcase_fields
-			if categorie
+			if categorie.save
 				render :json => categorie, status: :ok
 			else
 				render :json => categorie, status: :bad_request
@@ -15,6 +15,12 @@ class Api::V1::Categories::CategoriesController < ApplicationController
 
 	def index
 		render :json => {model: Categorie.all.where(categorie_type: params[:type])}, status: :ok
+	end
+
+	def show
+		response = {categorie: Categorie.find(params[:id]), subcategories:
+			Categorie.all.where(categorie_type: 1, categorie_id: params[:id])}
+		render :json => response, status: :ok
 	end
 
 	def search_subcategorie
