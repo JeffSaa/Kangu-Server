34886e9a15aef324c5edaf0f6e34ff524fd7b723
Jeff <jeffreysaa@outlook.com>
2017-06-16 13:23:20 -0700

    CloseDay fix

--- app/controllers/api/v1/administration/accounting_controller.rb
+++ app/controllers/api/v1/administration/accounting_controller.rb
@@ -2,11 +2,13 @@ class Api::V1::Administration::AccountingController < ApplicationController
 	before_action :validate_authentification_token
 
 	def close_day
-		response = []
+		response = {total: 0, model: []}
 		day = DateTime.parse params[:day]
 		orders = Order.where(status: 3).where('datehour BETWEEN ? AND ?', day.beginning_of_day, day.end_of_day)
 		orders.each do |o|
-			response << {order: o, order_products: OrderProduct.where(order_id: o.id)}
+			products = OrderProduct.where(order_id: o.id)
+			products.each{|p| response[:total] += p.price}
+			response[:model] << {order: o, order_products: products}
 		end
 		render :json => response, status: :ok
 	end
