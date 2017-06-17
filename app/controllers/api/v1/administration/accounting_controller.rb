3c679f5d950b2741429831c9868b749ceb62ef3b
Jeff <jeffreysaa@outlook.com>
2017-06-17 11:40:40 -0700

    CloseDay ready

--- app/controllers/api/v1/administration/accounting_controller.rb
+++ app/controllers/api/v1/administration/accounting_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::Administration::AccountingController < ApplicationController
 		orders = Order.where(status: 3).where('datehour BETWEEN ? AND ?', day.beginning_of_day, day.end_of_day)
 		orders.each do |o|
 			products = OrderProduct.where(order_id: o.id)
-			products.each{|p| response[:total] += p.price}
+			response[:total] += o.total
 			response[:model] << {order: o, order_products: products}
 		end
 		render :json => response, status: :ok
