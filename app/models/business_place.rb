4df500ec2a6c4e2b899b2e5ce272093fd71b7bc2
Jeff <jeffreysaa@outlook.com>
2017-07-07 20:23:45 -0500

    Add credit_status method

--- app/models/business_place.rb
+++ app/models/business_place.rb
@@ -5,4 +5,12 @@ class BusinessPlace < ApplicationRecord
 		self.name = name.downcase
 	end
 
+	def credit_status
+		BusinessPlace.where('current_deb > ?', 0).each do |bp|
+			user = getPlaceOwner(bp.id)
+			ConvertLoop.people.create_or_update(email: user.email, first_name: user.name, last_name: user.lastname, total: 0, token: bp.uid)
+			ConvertLoop.event_logs.send(name: "credit-status", person: { email: user.email })
+		end
+	end
+
 end
