ccefac3c673bf43a3b680510ac342684d7810363
Jeff <jeffreysaa@outlook.com>
2017-07-31 15:56:19 -0500

    Mailer ready on order_interest and credit_status method

--- app/models/business_place.rb
+++ app/models/business_place.rb
@@ -5,11 +5,13 @@ class BusinessPlace < ApplicationRecord
 		self.name = name.downcase
 	end
 
-	def credit_status
+	def self.credit_status
 		BusinessPlace.where('current_deb > ?', 0).each do |bp|
-			user = getPlaceOwner(bp.id)
-			ConvertLoop.people.create_or_update(email: user.email, first_name: user.name, last_name: user.lastname, total: 0, token: bp.uid)
-			ConvertLoop.event_logs.send(name: "credit-status", person: { email: user.email })
+			Charge.where(target_id: bp.id, type_id: Constants::BUSINESS_OWNER).each do |ch|
+				user = User.find(ch.user_id)
+				ConvertLoop.people.create_or_update(email: user.email, first_name: user.name, last_name: user.lastname, total: bp.current_deb, token: bp.uid)
+				ConvertLoop.event_logs.send(name: "credit-status", person: { email: user.email })
+			end
 		end
 	end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,16 @@
 class Order < ApplicationRecord
+
+	def self.interest_order
+		Order.where(order_type: 0, is_payed: false, next_interest_day: Date.today).each do |o|
+			o.update(interest_count: o.interest_count + 1, next_interest_day: o.next_interest_day + 30)
+			sucursal = BusinessSucursal.find(o.target_id)
+			place = BusinessPlace.find(sucursal.business_id)
+			Charge.where(target_id: place.id, type_id: Constants::BUSINESS_OWNER).each do |ch|
+				user = User.find(ch.user_id)
+				ConvertLoop.people.create_or_update(email: user.email, first_name: user.name, last_name: user.lastname, total: 0, token: place.uid)
+				ConvertLoop.event_logs.send(name: "order-interest", person: { email: user.email })
+			end
+		end
+	end
+
 end
